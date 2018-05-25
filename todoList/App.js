398132f6825944a2924a4d0e82721d9ad375a2eb
İbrahim Şakar <ibrahimsakar@Ibrahim-MacBook-Pro.local>
2018-05-25 23:28:03 +0300

    App completed.

--- todoList/App.js
+++ todoList/App.js
@@ -9,50 +9,64 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  ScrollView,
+  TextInput,
+  Button
 } from 'react-native';
+import MyButton from './button';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const items = [];
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+export default class App extends Component<{}> {
+
+  constructor(props) {
+    super(props);
+    
+    this.addItem = this.addItem.bind(this);
+  }
+
+  state = {
+    toDo: ''
+  }
+
+  addItem() {
+
+    items.push(this.state.toDo)
+
+    this.setState({ toDo: '' })
+  }
+
+  renderItem(item){
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+      <View key={ item } style={{ backgroundColor: 'gray', height: 100, margin: 8, borderRadius: 16, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{color: 'white', fontSize: 24 }}>
+         { item } 
         </Text>
       </View>
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  render() {
+    return (
+      <View style={{ flex: 1, marginTop: Platform.OS == 'ios' ? 21 : 0 }}>
+        <View style={{ backgroundColor: 'yellow', height: 100, flexDirection: 'row', padding: 8}}>
+          <View style={{ flex: 4, marginRight: 8 }}>
+            <TextInput value={ this.state.toDo } onChangeText={ (value) => this.setState({ toDo: value }) } placeholder={'Write here what you want'} style={{ backgroundColor: '#f4f4f4', height: 80, borderRadius: 12, paddingLeft: 8 }} />
+          </View>
+
+          <View style={{ backgroundColor: 'white', flex: 1 }}>
+            <MyButton onPress={ this.addItem } text={'Add'} />
+          </View>
+        </View>
+
+        <ScrollView>
+          {
+            items.map((item) => this.renderItem(item))
+          }
+        </ScrollView>
+
+      </View>
+    );
+  }
+}
